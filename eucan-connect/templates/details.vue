d026d7c7fa5c73a1afffab210c8c882d4293ec77
Esther van Enckevort <esther@vanenckevort.net>
2021-04-21 10:40:03 +0200

    First version of interface design

---
+++ eucan-connect/templates/details.vue
@@ -0,0 +1,51 @@
+<h2>{{record.study_name}}</h2>
+<button data-v-5175c9b1="" type="button" class="btn-sm float-right text-nowrap shopping-button btn-outline-secondary"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-plus fa-w-14 fa-xs"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" class=""></path></svg><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shopping-cart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-shopping-cart fa-w-18"><path fill="currentColor" d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z" class=""></path></svg></button>
+<p>Acronym: {{record.acronym}}</p>
+<p>{{record.objectives}}</p>
+<p>Start year: {{record.start_year}}</p>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <h2>Maelstrom</h2>
+      <p><a href="https://www.maelstrom-research.org/page/catalogue" target="_blank">Go to {{record.study_name}} in the Maelstrom Catalogue.</a></p>
+<input type="checkbox" id="module1" name="module1" value="module1" checked>
+<label for="module1">Basic descriptive data</label><br>
+<input type="checkbox" id="module2" name="module2" value="module2" checked>
+<label for="module2">Detailed descriptive data</label><br>
+<input type="checkbox" id="module3" name="module3" value="module3" checked>
+<label for="module3">Information domains</label><br>
+<input type="checkbox" id="module4" name="module4" value="module4" checked>
+<label for="module4">List of variables collected</label><br>
+<input type="checkbox" id="module5" name="module5" value="module5" checked>
+<label for="module5">Summary statistics</label><br>
+    </div>
+    <div class="col-sm">
+      <h2>Birthcohorts.net</h2>
+      <p><a href="https://www.birthcohorts.net/" target="_blank">Go to {{record.study_name}} in the Birthcohorts.net Catalogue.</a></p>
+<input type="checkbox" id="module1" name="module1" value="module1" checked>
+<label for="module1">Basic descriptive data</label><br>
+<input type="checkbox" id="module2" name="module2" value="module2" checked>
+<label for="module2">Detailed descriptive data</label><br>
+<input type="checkbox" id="module3" name="module3" value="module3" checked>
+<label for="module3">Information domains</label><br>
+<input type="checkbox" id="module4" name="module4" value="module4">
+<label for="module4">List of variables collected</label><br>
+<input type="checkbox" id="module5" name="module5" value="module5">
+<label for="module5">Summary statistics</label><br>
+    </div>
+    <div class="col-sm">
+      <h2>BBMRI-ERIC Directory</h2>
+      <p><a href="https://directory.bbmri-eric.eu/" target="_blank">Go to {{record.study_name}} in the BBMRI-ERIC Directory.</a></p>
+<input type="checkbox" id="module1" name="module1" value="module1" checked>
+<label for="module1">Basic descriptive data</label><br>
+<input type="checkbox" id="module2" name="module2" value="module2" checked>
+<label for="module2">Detailed descriptive data</label><br>
+<input type="checkbox" id="module3" name="module3" value="module3">
+<label for="module3">Information domains</label><br>
+<input type="checkbox" id="module4" name="module4" value="module4">
+<label for="module4">List of variables collected</label><br>
+<input type="checkbox" id="module5" name="module5" value="module5">
+<label for="module5">Summary statistics</label><br>
+    </div>
+  </div>
+</div>
